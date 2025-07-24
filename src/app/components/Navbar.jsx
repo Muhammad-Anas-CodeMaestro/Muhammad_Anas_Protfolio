da61187e06c8c67bc322f647737d753a28473544
Muhammad-Anas-CodeMaestro <mraza50044@gmail.com>
2025-07-24 11:56:11 +0500

    Update Navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,19 +3,21 @@ import React from 'react'
 function Navbar() {
     return (
         <>
-            <div className='flex justify-between'>
-                <div>
-                    <h3>A</h3>
-                    <h1>Anas</h1>
-                </div>
-                <div>
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Protfolio</li>
-                        <li>Service</li>
-                        <li>Contact</li>
-                    </ul>
+            <div className='h-18'>
+                <div className='flex justify-between h-18 w-3/4 mx-auto items-center'>
+                    <div className='flex items-center pl-3 pr-3'>
+                        <h3 className='bg-purple-500 font-medium text-white px-5 py-3 rounded-full text-2xl mr-3'>A</h3>
+                        <h3 className='text-gray-900 text-2xl font-bold'>Anas</h3>
+                    </div>
+                    <div className='w-2/4'>
+                        <ul className='flex justify-around items-center text-gray-900 text-lg'>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Protfolio</li>
+                            <li>Services</li>
+                            <li className='px-5 py-2 bg-purple-500 text-white rounded-md'>Contact</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </>
