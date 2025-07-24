1203d5aef79707ad8dbba4d04ac83db9456ba50f
Muhammad-Anas-CodeMaestro <mraza50044@gmail.com>
2025-07-24 10:04:33 +0500

    update project structure and dependencies; add Navbar component and modify layout

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <div className='flex justify-between'>
+                <div>
+                    <h3>A</h3>
+                    <h1>Anas</h1>
+                </div>
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Protfolio</li>
+                        <li>Service</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
