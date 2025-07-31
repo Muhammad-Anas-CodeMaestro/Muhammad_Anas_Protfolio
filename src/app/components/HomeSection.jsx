5e91f47694bdef504344ac56477aebbf67ec2377
Muhammad-Anas-CodeMaestro <mraza50044@gmail.com>
2025-07-31 19:34:07 +0500

    Uptodate HeroSection

--- src/app/components/HomeSection.jsx
+++ src/app/components/HomeSection.jsx
@@ -20,27 +20,27 @@ function HomeSection() {
       <div className="relative w-full min-h-screen flex items-center justify-center">
         <div className='bg-gradient-to-l from-purple-200 to-white absolute right-0 top-0 w-1/2 h-full z-0'></div>
         <div className="w-2/3 mx-auto relative z-10">
-          <div className=" flex justify-between items-center h-fit">
+          <div className=" flex justify-between items-center h-fit mt-12 mb-10">
             <div className="w-1/2 px-3">
               <h1 className="text-5xl text-gray-900 py-5">Hello, I'm Muhammad Anas</h1>
               <p className="w-full text-gray-600 text-sm drop-shadow-lg pb-5">
                 I am a  <span className="relative font-semibold text-gray-900">
                   <span className="absolute inset-x-0 bottom-0 h-1/2 bg-pink-200 z-[-1]"></span>
                   Front-End Developer
-                </span> trying his best to build clean, responsive, and, of course, accessible user interface. Bringing ideas to the visions of interactivity with pixel perfect websites.
+                </span> trying my best to build clean, responsive, and, of course, accessible user interface. Bringing ideas to the visions of interactivity with pixel perfect websites.
               </p>
               <h3 className="px-5 py-2 bg-purple-500 text-white rounded-md w-fit">Say Hello!</h3>
-              <div className="flex justify-around items-center mt-20 mb-10 py-3 text-center bg-gray-300">
+              <div className="flex justify-around items-center mt-20 mb-10 text-center ">
                 {data.map((item, index) => (
-                  <div key={index} className="">
-                    <h3 className="">{item.h3}</h3>
-                    <h5 className="">{item.h5}</h5>
+                  <div key={index} className="bg-gray-200 w-full mx-0.5 py-2">
+                    <h3 className="text-gray-800 text-lg font-semibold pb-1.5">{item.h3}</h3>
+                    <h5 className="text-gray-600 text-sm">{item.h5}</h5>
                   </div>
                 ))}
               </div>
             </div>
-            <div className="">
-              <img src="/DP.jpg" alt="AnasPicture" width={300} />
+            <div className="w-2/5">
+              <img src="/DP.jpg" alt="AnasPicture" width={300} className='rounded-2xl  shadow-lg'/>
             </div>
           </div>
           <div className="bg-yellow-500">
